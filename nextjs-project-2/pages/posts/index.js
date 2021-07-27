4d8731655b57ee95f6b262b3752a646dbe2e1a68
Chandan Basumatary <cbasumat@in.ibm.com>
2021-07-27 16:35:22 +0530

    Add All Posts page

--- nextjs-project-2/pages/posts/index.js
+++ nextjs-project-2/pages/posts/index.js
@@ -1,5 +1,35 @@
-function AllPostsPage() {
+import AllPosts from "../../components/posts/all-posts";
 
+const DUMMY_POSTS = [
+    // {title, image, excerpt, date, slug}
+    { 
+        title: 'NextJS', 
+        image: 'NextJS.png', 
+        excerpt: 'Wonderful adventures', 
+        date: '2021-08-20', 
+        slug: 'Building my blog1' 
+    },
+    { 
+        title: 'NextJS', 
+        image: 'NextJS.png', 
+        excerpt: 'Wonderful adventures', 
+        date: '2021-08-20', 
+        slug: 'Building my blog2' 
+    },
+    { 
+        title: 'NextJS', 
+        image: 'NextJS.png', 
+        excerpt: 'Wonderful adventures', 
+        date: '2021-08-20', 
+        slug: 'Building my blog3' 
+    }
+];
+
+
+function AllPostsPage() {
+    return (
+        <AllPosts posts={DUMMY_POSTS} />
+    );
 }
 
 export default AllPostsPage;
