a5d1a89b370ca2b8e34c09da1da8be1da43320ba
Chandan Basumatary <cbasumat@in.ibm.com>
2021-07-27 13:12:40 +0530

    Read DEV secret from file

--- nextjs-project-2/next.config.js
+++ nextjs-project-2/next.config.js
@@ -1,3 +1,28 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase) => {
+  const fs = require('fs');
+  const path = require('path');
+
+  if( phase === PHASE_DEVELOPMENT_SERVER ) {
+    const fPath = path.join( __dirname , '../', '../', '../', 'Secrets', 'NextJS-App2-Secrets.json');
+    const fContents = fs.readFileSync(fPath, 'utf-8');
+    // console.log( 'FileConetents:\n' + fContents);
+    const mySecrets = JSON.parse(fContents);
+    // console.log( 'Secrets:\n' + mySecrets);
+    return {
+      env: {
+        MONGODB_DRIVER: mySecrets.MONGODB_DRIVER,
+        MONGODB_USERNAME: mySecrets.MONGODB_USERNAME,
+        MONGODB_PASSWORD: mySecrets.MONGODB_PASSWORD,
+        MONGODB_CLUSTER: mySecrets.MONGODB_CLUSTER,
+        MONGODB_DATABASE: mySecrets.MONGODB_DATABASE,
+        MONGODB_OTHER_PARAMS: mySecrets.MONGODB_OTHER_PARAMS,
+      },
+    };
+  }
+};
