6937a5e15423b9ee36440d2fa0522302e0b5ce79
Chandan Basumatary <cbasumat@in.ibm.com>
2021-07-28 00:27:07 +0530

    Add Head Data

--- nextjs-project-2/pages/contact.js
+++ nextjs-project-2/pages/contact.js
@@ -1,8 +1,16 @@
+import Head from 'next/head';
+import { Fragment } from "react";
 import ContactForm from "../components/contact/contact-form";
 
 function ContactPage() {
     return (
-        <ContactForm />
+        <Fragment>
+            <Head>
+                <title>Contact Me</title>
+                <meta name='description' content='Send me your messages' />
+            </Head>
+            <ContactForm />
+        </Fragment>
     );
 }
 
--- nextjs-project-2/pages/index.js
+++ nextjs-project-2/pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import { Fragment } from "react";
 import FeaturedPosts from "../components/home-page/featured-posts";
 
@@ -8,6 +9,10 @@ import { getFeaturedPosts } from "../lib/posts-util";
 function HomePage(props) {
     return (
         <Fragment>
+            <Head>
+                <title>Basu's Blog</title>
+                <meta name='description' content='I post about programming and web development.' />
+            </Head>
             <Hero />
             <FeaturedPosts posts={props.posts}/>
         </Fragment>
